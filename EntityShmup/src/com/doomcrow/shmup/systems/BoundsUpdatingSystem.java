f0758fb27483755320944a74f8832f80a281bee5
Justin Hamilton <justinanthonyhamilton@gmail.com>
2013-07-19 11:26:32 -0700

    Adding a BoundsUpdatingSystem

---
+++ EntityShmup/src/com/doomcrow/shmup/systems/BoundsUpdatingSystem.java
@@ -0,0 +1,29 @@
+package com.doomcrow.shmup.systems;
+
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.annotations.Mapper;
+import com.artemis.systems.EntityProcessingSystem;
+import com.doomcrow.shmup.components.Bounds;
+import com.doomcrow.shmup.components.Position;
+
+public class BoundsUpdatingSystem extends EntityProcessingSystem {
+  @Mapper ComponentMapper<Position> positionMapper;
+  @Mapper ComponentMapper<Bounds> boundsMapper;
+  
+
+  @SuppressWarnings("unchecked")
+  public BoundsUpdatingSystem() {
+    super(Aspect.getAspectForAll(Bounds.class, Position.class));
+  }
+  
+  @Override
+  protected void process(Entity entity) {
+    Position position = positionMapper.get(entity);
+    Bounds bounds = boundsMapper.get(entity);
+    
+    bounds.rect.x = position.pos.x;
+    bounds.rect.y = position.pos.y;
+  }
+}
